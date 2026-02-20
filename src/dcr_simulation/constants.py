736ee7e1f2b5d6cb00da1de2385827d785898df0
Thomas Bourke <thomas.bourke1@outlook.com>
2026-02-20 17:49:14 +0000

    add constants.py

--- src/dcr_simulation/constants.py
+++ src/dcr_simulation/constants.py
@@ -1,5 +1,5 @@
 """
-Contains mathematical and physical constants
+Contains physical constants and experimental parameters
 
 Features:
 - 
@@ -13,4 +13,15 @@ Features:
 
 # -----------------------------
 
+
+# Physical constants
 ELECTRON_CHARGE =   1.602e-19 # Coulombs
+
+# Experimental parameters
+P_a = 0.1 # Avalanche probability (could also find from McIntyre model) / probability
+tau = 2e-9 # Gate pulse width / s
+M_0 = 10 #DC gain
+GB = 30e9 # Gain bandwidth / Hz
+c= 0.01 # Radio fo trapped carriers to the total carriers generated per avalanche pulse
+M_g = 1e8 # Gain when gate applied I think?
+delta_T = 1e-5 # Reciprocal of pulse repetiion rate 1GHz = 1ns
